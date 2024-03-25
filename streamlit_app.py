55223812afaf62f017fb3e4b0f322f126233c1f1
Mugdha2406 <94961578+Mugdha2406@users.noreply.github.com>
2024-03-25 17:26:26 +0530

    Update streamlit_app.py
    loop change

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,8 +27,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 # st.stop()
 pd_df = my_dataframe.to_pandas()
 # pd_df = pd.DataFrame(my_dataframe)
-# st.dataframe(pd_df)
-# st.stop()                                                                      
+st.dataframe(pd_df)
+st.stop()                                                                      
 
 ingredients_list = st.multiselect('Choose upto 5 ingredients:', my_dataframe, max_selections = 5)
 
@@ -39,16 +39,14 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-        # search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        # st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
 
         st.subheader(fruit_chosen + ' Nutrition Information')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
         fv_df = st.dataframe(data = fruityvice_response.json(), use_container_width=True)
 
-    # st.write(ingredients_string)
-
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
+        my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
             values ('""" + ingredients_string + """','""" + name_on_order + """')"""
   
     time_to_insert = st.button('Submit Order')
