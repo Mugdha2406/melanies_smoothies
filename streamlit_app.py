2a3bca0ea7e24c3cc7aed42527337d14cd62f64d
Mugdha2406 <94961578+Mugdha2406@users.noreply.github.com>
2024-03-25 17:31:00 +0530

    Update streamlit_app.py
    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,7 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        # search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
 
         st.subheader(fruit_chosen + ' Nutrition Information')
@@ -51,7 +51,7 @@ if ingredients_list:
   
     time_to_insert = st.button('Submit Order')
 
-    # st.write(my_insert_stmt)
+    st.write(my_insert_stmt)
     # if ingredients_string:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
